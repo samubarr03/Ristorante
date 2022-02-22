1c2a0609beda7c9a625453391ef1dd6a26f3584e
samubarr03 <98096249+samubarr03@users.noreply.github.com>
2022-02-22 13:44:43 +0100

    main

--- index.php
+++ index.php
@@ -1,345 +1,191 @@
 <html lang="en">
-        <head>
-            <!-- Required meta tags -->
-            <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-            <!-- Bootstrap CSS -->
-            <link rel="stylesheet" href="css/bootstrap.min.css">
-            <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+		<head>
+			<!-- Required meta tags -->
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+			<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+			<!-- Bootstrap CSS -->
+			<link rel="stylesheet" href="css/bootstrap.min.css">
+			<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+			<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>-->
+    		<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+			<script src="js/bootstrap.bundle.min.js"></script>
+			<!-- Development version -->
+			<script src="https://unpkg.com/@popperjs/core@2/dist/umd/popper.js"></script>
+			<title>Prenota-One</title>
+			<link rel="stylesheet" href="css/bootstrap.min.css">
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	  	</head>
+			<style>
+		  		@import url(style.css);
+				  .dropbtn {
+					background-color: #ff8733;
+					color: white;
+					padding: 16px;
+					font-size: 24px;
+					border: none;
+					cursor: pointer;
+					width: 160px;
+					height: 80px;
+					}
 
+					.dropdown {
+					position: relative;
+					display: inline-block;
+					}
 
-            <!-- LINK FOOTER -->
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-            <link rel="stylesheet" href="css/assets/style.css">
-            <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>-->
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-            <script src="js/bootstrap.bundle.min.js"></script>
-            <!-- Development version -->
-            <script src="https://unpkg.com/@popperjs/core@2/dist/umd/popper.js"></script>
-            <title>Prenota-One</title>
-        </head>
-        <style>
-            .centrare{
-                padding-left:8%;
-                padding-right:8%;
-                display: inline-block;
-            }
-            .bg{
-                z-index: -1;
-                background: repeating-linear-gradient(
-                45deg,
-                #ff6900,
-                #ff6900 40px,
-                #ff8733 40px,
-                #ff8733 80px
-                );
-            }
-            .button {
-                background-color: #ff8733;
-                border: none;
-                color: white;
-                padding: 15px 32px;
-                text-align: center;
-                text-decoration: none;
-                display: inline-block;
-                float:center;
-                font-size: 280%;
-                margin: 4px 2px;
-                margin-left:2%;
-                margin-right:2%;
-                cursor: pointer;
-                width: 28%;  
-                height: 10%;
-                font-family: "Open Sans", sans-serif;
-            }
-            .buttonfooter {
-                background-color: #ff6900;
-                border: none;
-                color: white;
-                padding: 15px 32px;
-                text-align: center;
-                text-decoration: none;
-                display: inline-block;
-                float:center;
-                font-size: 180%;
-                margin: 4px 2px;
-                margin-left:2%;
-                margin-right:2%;
-                cursor: pointer;
-                width: 75%;  
-                height: 70%;
-            }.buttonfooterb{
-                background-color: #ff6900;
-                border: none;
-                color: white;
-                padding: 15px 32px;
-                text-align: center;
-                text-decoration: none;
-                display: inline-block;
-                float:center;
-                font-size: 180%;
-                margin: 4px 2px;
-                margin-left:2%;
-                margin-right:2%;
-                cursor: pointer;
-                width: 97%;  
-                height: 70%;
-            }
-            .bottonialcentro{
-                background-color:#ff6900;
-                margin-left:22.5%;
-                margin-right:22.5%;
-                padding-left:1%;
-                padding-right:1%;
-                padding-bottom:0.75%;
-                padding-top:0.75%;
-                text-align: center;
-                position: relative;
-                z-index: 0;
-                top:-10%;
-            }
-            .flex {max-width: 89%;
-                position: relative;
-                z-index: 0;
-                margin-left:5.5%;
-                margin-right:5.5%;
-                border-radius: 0.1%;
-            }
-            .bottonialfooter{
-                text-align: center;
-                padding-left:1%;
-                padding-right:1%;
-                padding-bottom:0.75%;
-                padding-top:0.75%;
-                text-align: center;
-                position: relative;
-                z-index: 0;
-                top:-10%;
-            }
-            .parent {
-                text-align: center;
-                margin: 20px 0;
-                padding: 20px;
-                }
+					.dropdown-content {
+					display: none;
+					position: absolute;
+					background-color: #ff8733;
+					min-width: 160px;
+					box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+					z-index: 1;
+					}
 
-            .parent div {
-                margin:10px;
-                padding: 10px;
-                display: inline-block;
-                text-align: left;
-            }
-            .pparte{
-                background-color:#ff8733;
-                text-align: center;
-                padding-left:20%;
-                padding-right:20%;
-            }
-            .pparte p {
-                font-variant-numeric: oldstyle-nums;
-                padding-left:10%;
-                padding-right:10%;
-            }
-            .pparte h1 {
-                font-variant-numeric: oldstyle-nums;
-                color: #000000;
-                padding-left:20%;
-                padding-right:20%;
-            }
-            .sparte{
-                background-color:#ff6900;
-                text-align: center;
-            }
-            .sparte p {
-                font-variant-numeric: oldstyle-nums;
-                padding-left:10%;
-                padding-right:10%;
-            }
-            .sparte h1 {
-                font-variant-numeric: oldstyle-nums;
-                padding-left:20%;
-                padding-right:20%;
-            }
-            .card {
-                box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-                transition: 0.3s;
-                width: 66%;
-            }
-            .card:hover {
-                box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-            }
-            .allinea{
-                display: inline;
-            }
-            .piatti a{
-                display: inline-block;
-                width: 512px;
-                height: 512px;
-                color: #ff6900;
-                vertical-align: middle;
-                text-decoration: none;
-                font-size: 400%;
-                border: .5em solid rgba(0,0,0,0);
-                border-radius: 100%;
-                background: rgb(245,245,245);
-                box-shadow:
-                inset 0 -6px 20px rgba(255,255,255,1),
-                inset 0 6px 20px rgba(0,0,0,.4),
-                0 4px 8px rgba(0,0,0,.9);
-                text-align: center;
-                position: relative;
-                .dropbtn {
-                background-color:#ff6900 ;
-                color: white;
+					.dropdown-content a {
+					color: white;
+					padding: 12px 16px;
+					text-decoration: none;
+					display: block;
+					}
 
-                border: none;
-                cursor: pointer;
-            }
-            .dropbtn {
-            background-color: #04AA6D;
-            color: white;
-            padding: 16px;
-            font-size: 16px;
-            border: none;
-            }
+					.dropdown-content a:hover {background-color: #ff6900}
 
-            .dropdown {
-            position: relative;
-            display: inline-block;
-            }
+					.dropdown:hover .dropdown-content {
+					display: block;
+					}
 
-            .dropdown-content {
-            display: none;
-            position: absolute;
-            background-color: #f1f1f1;
-            min-width: 160px;
-            box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-            z-index: 1;
-            }
-
-            .dropdown-content a {
-            color: black;
-            padding: 12px 16px;
-            text-decoration: none;
-            display: block;
-            }
-
-            .dropdown-content a:hover {background-color: #ddd;}
-
-            .dropdown:hover .dropdown-content {display: block;}
-
-            .dropdown:hover .dropbtn {background-color: #3e8e41;}
-</style>
-        </style>
-        <body>
-        <div class="bg">
+					.dropdown:hover .dropbtn {
+					background-color: #ff6900;
+					}
+        	</style>
+		  <body>
+		<div class="bg">
         <!--NAVBAR -->
-        
-        <nav class="navbar navbar-light" style="background-color: #ff6900;">
-        <a class="navbar-brand" href="#">
-            <img src="img/logo_large_b.png" width="333" height="108" class="d-inline-block align-top" alt="">
-        </a>
-        <div class="dropdown">
-        <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Dropdown button
-        </button>
-        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-
+		
+		<nav class="navbar navbar-light" style="background-color: #ff6900;">
+		<br><a class="navbar-brand" href="#">
+			<img src="img/logo_large_b.png" width="333" height="108" class="d-inline-block align-top" alt="">
+		</a>
+		<div class="dropdown">
+  <button class="dropbtn">Dropdown</button>
+  <div class="dropdown-content">
+  <a href="#">Link 1</a>
+  <a href="#">Link 2</a>
+  <a href="#">Link 3</a>
+  </div>
+  </div>
+  
+  <div class="dropdown">
+  <button class="dropbtn">Account</button>
+  <div class="dropdown-content">
+  <a href="#">Informazioni</a>
+  <a href="#">Modifica account</a>
+  <div class="dropdown-divider"></div>
+    <a class="dropdown-item" href="#">Logout</a>
+  </div>
+  		<!--
+		<div class="dropdown">
+		<button class="btn btn-secondary btn-lg dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			Dropdown button
+		</button>
+		<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+			<a class="dropdown-item" href="#">Action</a>
+			<a class="dropdown-item" href="#">Another action</a>
+			<a class="dropdown-item" href="#">Something else here</a>
+			<div class="dropdown-divider"></div>
+ 			<a class="dropdown-item" href="#">Separated link</a>
+		</div>
+		-->
 </div>
-    </nav>
-    <img src="img/immagina.png" class="flex">
-    <div class="bottonialcentro">
-            <a href="#" class="button">Menù </a>
-        <a href="#" class="button">Prenota</a>
-        <a href="#" class="button">Chi siamo</a>
-    </div>
-    <br><br>
-    <div class="piatti">
-        <a href="#" class="button">Primo <img src="img/primo.svg" width="80%" height="80%" padding-bottom="100px"></a>
-        <a href="#" class="button">Primo <img src="img/primo.svg" width="80%" height="80%"></a>
-        <a href="#" class="button">Primo <img src="img/primo.svg" width="80%" height="80%"></a><br><br>
-    </div>
-    </div>
-    </div>
-    <div class="pparte">
-        <br>
-        <img src="img/primo.svg">
-        <br><br>
-        <h1>WHERE'S THE BEST PLACE TO GET A BURGER IN BRIGHTON & HOVE?</h1>
-        <br>
-        <h4>We're a Hove based web design and development agency with a penchant for burgers. We started this project as a bit of fun, designed to showcase our mad skillz.</h3>
-        
-        <h4>We've spent the last few months visiting and reviewing some of Brighton's premier eating establishments and chuffing our way through countless burgers, fries, relishes and coleslaws.</h4>
-        <h4>Come and join us on our culinary caper.</h4>
-        <br>        
-    </div>
-    <div class="sparte">
-        <br>
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-sm">
-                    <div class="card">
-                        <img src="img/pizza.jpg" alt="Avatar" style="width:100%">
-                        <div class="container">
-                            <h4><b>John Doe</b></h4>
-                            <p>Architect & Engineer</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm">
-                    <center><div class="card">
-                        <img src="img/mc.jpg" alt="Avatar" style="width:100%">
-                        <div class="container">
-                        <h4><b>John Doe</b></h4>
-                        <p>Architect & Engineer</p>
-                        </div>
-                    </div></center>
-                </div>
-                <div class="col-sm">
-                    <div class="card">
-                        <img src="img/bibite.png" alt="Avatar" style="width:100%">
-                        <div class="container">
-                        <h4><b>John Doe</b></h4>
-                        <p>Architect & Engineer</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <br>    
-    </div>
-</div>  
+<div class="bg">
+</div><br>
+	</nav>
+	
+	<img src="img/immagina.png" class="flex">
+	<div class="bottonialcentro">
+			<a href="#" class="button">Menù </a>
+		<a href="#" class="button">Prenota</a>
+		<a href="#" class="button">Chi siamo</a>
+	</div>
+
+	<br><br>
+	<div class="piatti">
+		<a href="#" class="button">Primo <img src="img/primo.svg" width="80%" height="80%" padding-bottom="100px"></a>
+		<a href="#" class="button">Primo <img src="img/primo.svg" width="80%" height="80%"></a>
+		<a href="#" class="button">Primo <img src="img/primo.svg" width="80%" height="80%"></a><br><br>
+	</div>
+	</div>
+	</div>
+	<div class="pparte">
+		<br>
+		<img src="img/primo.svg">
+		<br><br>
+		<h1>WHERE'S THE BEST PLACE TO GET A BURGER IN BRIGHTON & HOVE?</h1>
+		<br>
+		<h4>We're a Hove based web design and development agency with a penchant for burgers. We started this project as a bit of fun, designed to showcase our mad skillz.</h3>
+		
+		<h4>We've spent the last few months visiting and reviewing some of Brighton's premier eating establishments and chuffing our way through countless burgers, fries, relishes and coleslaws.</h4>
+		<h4>Come and join us on our culinary caper.</h4>
+		<br>		
+	</div>
+	<div class="sparte">
+		<br>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-sm">
+					<div class="card">
+						<img src="img/pizza.jpg" alt="Avatar" style="width:100%">
+						<div class="container">
+							<h4><b>John Doe</b></h4>
+							<p>Architect & Engineer</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm">
+					<center><div class="card">
+						<img src="img/mc.jpg" alt="Avatar" style="width:100%">
+						<div class="container">
+						<h4><b>John Doe</b></h4>
+						<p>Architect & Engineer</p>
+						</div>
+					</div></center>
+				</div>
+				<div class="col-sm">
+					<div class="card">
+						<img src="img/bibite.png" alt="Avatar" style="width:100%">
+						<div class="container">
+						<h4><b>John Doe</b></h4>
+						<p>Architect & Engineer</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<br>	
+	</div>
+</div>	
     
-        <!-- Footer -->
-<footer class="page-footer font-small" style="background-color: #ff8733;">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm-2" style=padding-top:1%;><center><img src="img/logo_small_icon_only_inverted.png"  width="128" height="128" ></center> </div>
-            <div class="col-sm" style=padding-top:2%;><center><a href="#" class="buttonfooterb">About our food</a></center></div>
-            <div class="col-sm" style=padding-top:2%;><center><a href="#" class="buttonfooter">Aiuto</a></center></div>
-            <div class="col-sm" style=padding-top:2%;><center><a href="#" class="buttonfooter">Feedback</a></center></div>
-            <div class="col-sm" style=padding-top:2%;><center><a href="#" class="buttonfooterb">Seguici sui social</a></center></div>
-            <div class="col-sm-2" style=padding-top:1%;><center>⠀</center> </div>
-        </div>
-    </div>
-<!-- Footer Elements -->
-  <!--Grid row-->
-</div>
-<!-- Footer Elements -->
-
+		<!-- Footer -->
+	<footer class="page-footer font-small" style="background-color: #ff8733;">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-sm-2" style=padding-top:1%;><center><img src="img/logo_small_icon_only_inverted.png"  width="128" height="128" ></center> </div>
+				<div class="col-sm" style=padding-top:2%;><center><a href="#" class="buttonfooterb">About our food</a></center></div>
+				<div class="col-sm" style=padding-top:2%;><center><a href="#" class="buttonfooter">Aiuto</a></center></div>
+				<div class="col-sm" style=padding-top:2%;><center><a href="#" class="buttonfooter">Feedback</a></center></div>
+				<div class="col-sm" style=padding-top:2%;><center><a href="#" class="buttonfooterb">Seguici sui social</a></center></div>
+				<div class="col-sm-2" style=padding-top:1%;><center>⠀</center> </div>
+			</div>
+		</div>
+	</div>
 <!-- Copyright -->
 <div class="footer-copyright text-center py-3">© 2020 Copyright:
   <a href="#">SCV.com</a>
 </div>
-<!-- Copyright -->
-
 </footer>
-<!-- Footer --> 
-    </body>
-    </html>
+	</body>
+	<script src="js/bootstrap.bundle.min.js"></script>
+	</html>
